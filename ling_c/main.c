fa8a71b88cb3814a7ad7b9cdef6ead5e4eeff3bf
gabrielleanne <anneodontoufrn@gmail.com>
2023-08-28 21:18:48 -0300

    VERSÃO 0.2
    VERSÃO 0.2

--- ling_c/main.c
+++ ling_c/main.c
@@ -5,14 +5,18 @@
 
 void tela_menu_principal(void);
 void tela_sobre(void);
-
+void tela_bikes(void);
+void tela_promocoes(void);
 
 int main(void) {
-    setlocale(LC_ALL,"Portuguese");
+    setlocale(LC_ALL,"Portuguese_Brazil");
     tela_menu_principal();
     tela_sobre();
+    tela_bikes();
+    tela_promocoes();
     return 0;
 }
+
 
 void tela_menu_principal(void) {
     char op;
@@ -21,13 +25,13 @@ void tela_menu_principal(void) {
     printf("-------------------------------------------------\n");
     printf("*******************RENT A CAR********************\n");
     printf("-------------------------------------------------\n");
-    printf(" 1. MÃ“DULO BIKES DISPONÃVEIS ------------DIGITE 1\n");
-    printf(" 2. MÃ“DULO CLIENTE-----------------------DIGITE 2\n");
-    printf(" 3. MÃ“DULO PROMOÃ‡Ã•ES---------------------DIGITE 3\n");
-    printf(" 4. MÃ“DULO SOBRE NÃ“S---------------------DIGITE 4\n");
+    printf(" 1. MÓDULO BIKES DISPONÍVEIS ------------DIGITE 1\n");
+    printf(" 2. MÓDULO CLIENTE-----------------------DIGITE 2\n");
+    printf(" 3. MÓDULO PROMOÇÕES---------------------DIGITE 3\n");
+    printf(" 4. MÓDULO SOBRE NÓS---------------------DIGITE 4\n");
     printf(" 0. SAIR---------------------------------DIGITE 0\n");
     printf("\n");
-    printf("Escolha sua opÃ§Ã£o: ");
+    printf("Escolha sua opção: ");
     scanf("%c", &op);
     getchar();
     printf("\n");
@@ -42,11 +46,82 @@ void tela_sobre(void) {
     printf("            * * * * RENT A CAR * * * *\n");
     printf("\n");
     printf("  Programa desenvolvido na linguagem C para a   \n");
-    printf("  disciplina de ProgramaÃ§Ã£o do curso de Sistemas\n");
-    printf("  de InformaÃ§Ã£o. O programa serÃ¡ desenvolvido   \n");
+    printf("  disciplina de Programação do curso de Sistemas\n");
+    printf("  de Informação. O programa será desenvolvido   \n");
     printf("  para uma empresa de aluguel de bicicletas -   \n");
     printf("  a Rent a Bike.                                \n");
     printf("  Desenvolvedora: Anne Gabrielle!!!!!           \n");                  
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
+
+void tela_bikes(void) {
+
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------\n");
+    printf("*******************RENT A CAR********************\n");
+    printf("-------------------------------------------------\n");
+    printf(" 1. VER BIKES----------------------------DIGITE 1\n");
+    printf(" 2. ALUGUEL BIKE-------------------------DIGITE 2\n");
+    printf(" 3. DICAS--------------------------------DIGITE 3\n");
+    printf(" 4. CADASTRAR BIKE-----------------------DIGITE 4\n");
+    printf(" 5. EDITAR BIKE--------------------------DIGITE 5\n");
+    printf(" 6. EXCLUIR BIKE-------------------------DIGITE 6\n");
+    printf(" 0. SAIR---------------------------------DIGITE 0\n");
+    printf("\n");
+    printf("Escolha sua opção: ");
+    scanf("%c", &op);
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+    
+void tela_clientes(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------\n");
+    printf("*******************RENT A CAR********************\n");
+    printf("-------------------------------------------------\n");
+    printf(" 1. CADASTRAR CLIENTE -------------------DIGITE 1\n");
+    printf(" 2. BUSCAR CLIENTE-----------------------DIGITE 2\n");
+    printf(" 3. EDITAR CADASTRO----------------------DIGITE 3\n");
+    printf(" 4. EXCLUIR CADASTRO---------------------DIGITE 4\n");
+    printf(" 5. LISTAR TODOS-------------------------DIGITE 5\n");
+    printf(" 0. SAIR---------------------------------DIGITE 0\n");
+    printf("\n");
+    printf("Escolha sua opção: ");
+    scanf("%c", &op);
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_promocoes(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------\n");
+    printf("*******************RENT A CAR********************\n");
+    printf("-------------------------------------------------\n");
+    printf(" 1. LISTAR PROMOÇÕES --------------------DIGITE 1\n");
+    printf(" 2. CADASTRAR PROMOÇÃO----=--------------DIGITE 2\n");
+    printf(" 3. EDITAR PROMOÇÃO----------------------DIGITE 3\n");
+    printf(" 4. EXCLUIR PROMOÇÃO---------------------DIGITE 4\n");
+    printf(" 0. SAIR---------------------------------DIGITE 0\n");
+    printf("\n");
+    printf("Escolha sua opção: ");
+    scanf("%c", &op);
+    getchar();
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+
+
